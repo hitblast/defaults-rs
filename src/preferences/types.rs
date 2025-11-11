1d79069081863827676bbac23d4e150b003f31e0
Anindya Shiddhartha <hitblastlive@gmail.com>
2025-11-11 23:48:21 +0600

    new types module for preferences

---
+++ src/preferences/types.rs
@@ -0,0 +1,34 @@
+/// Preferences domain (user or global).
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Domain {
+    /// A user domain, e.g., "com.apple.finder"
+    User(String),
+    /// The global preferences domain (".GlobalPreferences")
+    Global,
+}
+
+impl Domain {
+    /// Returns the CoreFoundation name for a given domain.
+    pub fn get_cf_name(&self) -> String {
+        match &self {
+            Domain::Global => String::from(".GlobalPreferences"),
+            Domain::User(name) => name.clone(),
+        }
+    }
+}
+
+impl std::fmt::Display for Domain {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Domain::User(s) => write!(f, "{}", s),
+            Domain::Global => write!(f, "NSGlobalDomain"),
+        }
+    }
+}
+
+/// Result of a find operation.
+#[derive(Debug)]
+pub struct FindMatch {
+    pub key_path: String,
+    pub value: String,
+}
